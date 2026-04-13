32bdcbd77951ca736e0fd17652236278b900d36d
Ishan Mahajan <im530@cam.ac.uk>
2026-04-14 00:20:38 +0100

    Adding static files - to complete video 2

--- fastapi_blog/main.py
+++ fastapi_blog/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
@@ -23,7 +25,7 @@ posts: list[dict] = [
     },
 ]
 
-@app.get("/", include_in_schema=False)
+@app.get("/", include_in_schema=False, name="home")
 def home(request: Request):
     return templates.TemplateResponse(request, "home.html", context={"posts": posts, "title": "Home"})
 
